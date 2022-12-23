fb1b7e111ae4ef7f355a86cdcb8902e85313d00b
theboatkitchen <fatieyizaty23@gmail.com>
2022-12-23 08:54:11 +0800

    backup 23/12/2022

---
+++ app/src/main/java/com/example/demoregister/Filter/RejectOrder.java
@@ -0,0 +1,14 @@
+package com.example.demoregister.Filter;
+
+public class RejectOrder {
+
+    //gender type
+    public static final String[] RejectReason ={
+            "Out of stock",
+            "Change my mind",
+            "other.."
+
+
+    };
+
+}
